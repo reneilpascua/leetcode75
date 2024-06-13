749eea962703b30a7e0c9a24c84f5ce5e5c69391
Reneil Pascua <reneilpascuatm@gmail.com>
2024-06-13 00:47:04 -0700

    lc 547; used recursive exploration with a set... accepted, but slow

---
+++ lc547_provinces.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+  def findCircleNum(self, isConnected: List[List[int]]) -> int:
+    # find number of disconnected graphs
+    # can't just process isConnected sequentially bc of indirect connections
+    
+    provinces = 0
+    seen = set()
+    
+    def explore(city_idx):
+      nonlocal seen, isConnected
+      
+      if city_idx in seen: return
+      seen.add(city_idx)
+
+      cxns = isConnected[city_idx]
+      for i, cxn in enumerate(cxns):
+        if cxn: explore(i)
+      
+    i = 0
+    while i < len(isConnected):
+      if i not in seen:
+        provinces += 1
+        explore(i)
+      i += 1
+    
+    return provinces
